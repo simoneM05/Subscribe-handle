06971c5e20796b817f0e5d9e03761c3c7fd6a4d8
Simone <61908241+simoneM05@users.noreply.github.com>
2025-04-08 23:03:32 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,25 @@ import "./config/redis"; // connect at redis client
 import { PORT } from "./config/config";
 import morgan from "morgan";
 import { logStream } from "./services/log.service";
+import rateLimit from "express-rate-limit";
 
 export const app = express();
+
+// Imposta un rate limit globale
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limita a 100 richieste per ogni IP
+  message: "Too many requests from this IP, please try again later.",
+  headers: true,
+});
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("combined", { stream: logStream }));
+
+// Applica il rate limit globalmente a tutte le rotte
+app.use(limiter);
 
 app.use("/api/users", UserRoutes);
 app.use("/api/subs", SubRoutes);
@@ -23,6 +35,7 @@ app.use(errorHandler);
 app.get("/", (req, res) => {
   res.json({ message: "API Running 🚀" });
 });
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
